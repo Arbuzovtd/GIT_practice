1662890dadc35d6e1a16c7290e3d4316e7298c36
Тимур Арбузов <arbuzovtd@gmail.com>
2024-08-22 18:20:06 +0300

    add venv

--- git_lecture.py
+++ git_lecture.py
@@ -5,3 +5,16 @@ def sample():
 if a:
     pass
 x = 100
+def test(mark):
+    if mark == 2:
+        print('f')
+    elif mark == 3:
+        print('C')
+    elif mark == 4:
+        print('B')
+    elif mark == 5:
+        print('A')
+    else:
+        print('error')
+for i in range(x):
+    print(i)
